aeebebf79e7ab07eb355cb706a2ea70a593d7b7f
Ryo Martin <laetuzg@gmail.com>
2023-04-21 17:17:42 +0700

    Added story response to become entity

--- app/src/main/java/com/neotica/storyapp/di/AppModule.kt
+++ app/src/main/java/com/neotica/storyapp/di/AppModule.kt
@@ -28,4 +28,3 @@ val networkModule = module {
         retrofit.create(ApiService::class.java)
     }
 }
-
--- app/src/main/java/com/neotica/storyapp/ui/response/Story.kt
+++ app/src/main/java/com/neotica/storyapp/ui/response/Story.kt
@@ -1,12 +1,16 @@
 package com.neotica.storyapp.ui.response
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity("story_entity")
 @Parcelize
 data class Story(
     val ava: String?,
     val name: String,
+    @PrimaryKey
     val id: String,
     val description: String,
     val photoUrl: String,
