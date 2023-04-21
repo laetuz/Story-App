ace39e8bf3522dc7510ad17ddb6edabd662a850d
Ryo Martin <laetuzg@gmail.com>
2023-04-21 17:02:45 +0700

    Added configurations for orientations.

--- app/src/main/java/com/neotica/storyapp/util/Utils.kt
+++ app/src/main/java/com/neotica/storyapp/util/Utils.kt
@@ -40,10 +40,66 @@ fun rotateImage(img: Bitmap, degree: Int): Bitmap {
 }
 
 @Throws(IOException::class)
+fun handleSamplingAndRotationBitmap(context: Context, selectedImage: Uri): Bitmap {
+    val MAX_HEIGHT = 1024
+    val MAX_WIDTH = 1024
+    val options = BitmapFactory.Options().apply {
+        inJustDecodeBounds = true
+    }
+    context.contentResolver.openInputStream(selectedImage)?.use { imageStream ->
+        BitmapFactory.decodeStream(imageStream, null, options)
+    }
+
+// Calculate inSampleSize
+    options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
+
+// Decode bitmap with inSampleSize set
+    options.inJustDecodeBounds = false
+    var img = context.contentResolver.openInputStream(selectedImage)?.use { imageStream ->
+        BitmapFactory.decodeStream(imageStream, null, options)
+    }
+
+    img = rotateImageIfRequired(img!!, selectedImage)
+    return img
+}
+
+private fun calculateInSampleSize(
+    options: BitmapFactory.Options,
+    reqWidth: Int, reqHeight: Int
+): Int {
+    // Raw height and width of image
+    val height = options.outHeight
+    val width = options.outWidth
+    var inSampleSize = 1
+
+    if (height > reqHeight || width > reqWidth) {
+        // Calculate ratios of height and width to requested height and width
+        val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
+        val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
+
+        // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+        // with both dimensions larger than or equal to the requested height and width.
+        inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
+
+        val totalPixels = (width * height).toFloat()
+        // Anything more than 2x the requested pixels we'll sample down further
+        val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
+
+        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+            inSampleSize++
+        }
+    }
+    return inSampleSize
+}
+
+@Throws(IOException::class)
 fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap {
     val ei = ExifInterface(selectedImage.path!!)
 
-    return when (ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
+    return when (ei.getAttributeInt(
+        ExifInterface.TAG_ORIENTATION,
+        ExifInterface.ORIENTATION_NORMAL
+    )) {
         ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90)
         ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180)
         ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270)
@@ -94,7 +150,7 @@ fun uriToFile(selectedImg: Uri, context: Context): File {
     return myFile
 }
 
-fun reduceFileImage(file: File): File{
+fun reduceFileImage(file: File): File {
     val bitmap = BitmapFactory.decodeFile(file.path)
     var compressQuality = 100
     var streamLength: Int
@@ -114,4 +170,3 @@ fun formatDateTime(isoDateString: String): String {
     val dateFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
     return dateFormatter.format(dateTime)
 }
-
